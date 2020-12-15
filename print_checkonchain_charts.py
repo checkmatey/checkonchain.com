d2e94258fa2790d743bf0a9ba55e43281c6ae68f
checkmatey <checkm8ey@protonmail.com>
2020-12-15 20:47:17 +1100

    formatting, scroll and monero

--- print_checkonchain_charts.py
+++ print_checkonchain_charts.py
@@ -1,9 +1,18 @@
 import os
 os.chdir('../dcronchain/charts')
 import checkonchain.dcronchain.charts.dcr_charts_plotting
+
+
 os.chdir(os.path.dirname(__file__)) #reset dir to this file
 os.chdir('../btconchain/charts')
 import checkonchain.btconchain.charts.btc_charts_plotting
+
+
+os.chdir(os.path.dirname(__file__)) #reset dir to this file
+os.chdir('../xmronchain')
+import checkonchain.xmronchain.xmr_charts
+
+
 #Print Research Paper Charts
 os.chdir(os.path.dirname(__file__)) #reset dir to this file
 os.chdir('../dcronchain/charts')
