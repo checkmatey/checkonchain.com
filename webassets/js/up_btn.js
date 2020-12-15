888c04b17df5170c9904328e6f9e5e75930a1b2a
checkmatey <checkm8ey@protonmail.com>
2020-12-15 22:06:47 +1100

    monero tx count charts, smooth scroll, top arrow

---
+++ webassets/js/up_btn.js
@@ -0,0 +1,19 @@
+up({
+    // bottom position
+    bottom: '20px',
+    // right position
+    right: '20px',
+    // width
+    width: '45px',
+    // height
+    height: '45px',
+    // background color
+    bg: 'green',
+    // custom icon
+    src: './chevron-up-solid.svg',
+    // distance from the top to show the back to top button
+    whenShow: 400,
+    // circular button?
+    circle: true
+    
+  });
