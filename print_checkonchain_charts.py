540788d670f6665b9f183fb9c1758cc747336629
checkmatey <checkm8ey@protonmail.com>
2020-12-15 17:59:50 +1100

    unforgeable costliness

--- print_checkonchain_charts.py
+++ print_checkonchain_charts.py
@@ -4,3 +4,7 @@ import checkonchain.dcronchain.charts.dcr_charts_plotting
 os.chdir(os.path.dirname(__file__)) #reset dir to this file
 os.chdir('../btconchain/charts')
 import checkonchain.btconchain.charts.btc_charts_plotting
+#Print Research Paper Charts
+os.chdir(os.path.dirname(__file__)) #reset dir to this file
+os.chdir('../dcronchain/charts')
+import checkonchain.dcronchain.charts.chart_dcr_unforgeablecostliness
