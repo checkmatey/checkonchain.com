5b55208f6f331dde906ab6ffdf2f22563ec288a8
checkmatey <checkm8ey@protonmail.com>
2021-04-11 10:45:03 +1000

    10/4

--- print_checkonchain_charts.py
+++ print_checkonchain_charts.py
@@ -3,21 +3,25 @@ import os
 #Print Private charts   = 0
 public  = 0
 
-
+#DECRED
 os.chdir(os.path.dirname(__file__)) #reset dir to this file
 os.chdir('../dcronchain/charts')
 import checkonchain.dcronchain.charts.dcr_charts_plotting
 
-
+#BITCOIN
 os.chdir(os.path.dirname(__file__)) #reset dir to this file
 os.chdir('../btconchain/charts')
 import checkonchain.btconchain.charts.btc_charts_plotting
 
-
+#MONERO
 os.chdir(os.path.dirname(__file__)) #reset dir to this file
 os.chdir('../xmronchain')
 import checkonchain.xmronchain.xmr_charts
 
+#ALTCOINS
+os.chdir(os.path.dirname(__file__)) #reset dir to this file
+os.chdir('../altonchain')
+import checkonchain.altonchain.alt_add_metrics
 
 #Print Research Paper Charts
 os.chdir(os.path.dirname(__file__)) #reset dir to this file
