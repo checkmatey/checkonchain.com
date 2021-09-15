e17c0511d33698185e7bfd5d462867e4faf42b0b
checkmatey <checkm8ey@protonmail.com>
2021-09-16 09:07:00 +1000

    15/9

--- print_checkonchain_charts.py
+++ print_checkonchain_charts.py
@@ -1,11 +1,4 @@
 from checkonchain.btconchain.btc_add_metrics import *
-import checkonchain.dcronchain.charts.chart_dcr_useradoption
-import checkonchain.dcronchain.charts.chart_dcr_unforgeablecostliness
-import checkonchain.stableonchain.charts.stable_charts_plotting
-import checkonchain.altonchain.alt_add_metrics
-import checkonchain.xmronchain.xmr_charts
-import checkonchain.dcronchain.charts.dcr_charts_plotting
-import checkonchain.btconchain.charts.btc_charts_plotting
 import os
 # Print Public Charts    = 1
 # Print Private charts   = 0
@@ -14,30 +7,33 @@ public = 0
 # BITCOIN
 os.chdir(os.path.dirname(__file__))  # reset dir to this file
 os.chdir('../btconchain/charts')
+import checkonchain.btconchain.charts.btc_charts_plotting
 
 # DECRED
 os.chdir(os.path.dirname(__file__))  # reset dir to this file
 os.chdir('../dcronchain/charts')
+import checkonchain.dcronchain.charts.dcr_charts_plotting
 
 # MONERO
 os.chdir(os.path.dirname(__file__))  # reset dir to this file
 os.chdir('../xmronchain')
+import checkonchain.xmronchain.xmr_charts
 
 # ALTCOINS
 os.chdir(os.path.dirname(__file__))  # reset dir to this file
 os.chdir('../altonchain')
+import checkonchain.altonchain.alt_add_metrics
 
 # STABLECOINS
 os.chdir(os.path.dirname(__file__))  # reset dir to this file
 os.chdir('../stableonchain')
+import checkonchain.stableonchain.charts.stable_charts_plotting
 
 # Print Research Paper Charts
 os.chdir(os.path.dirname(__file__))  # reset dir to this file
 os.chdir('../dcronchain/charts')
-
-# Print Research Paper Charts
-os.chdir(os.path.dirname(__file__))  # reset dir to this file
-os.chdir('../dcronchain/charts')
+import checkonchain.dcronchain.charts.chart_dcr_unforgeablecostliness
+import checkonchain.dcronchain.charts.chart_dcr_useradoption
 
 
 # Print Last Valid Timestamp
