031d5b4e533214f493b070b59675b49e2419009d
checkmatey <checkm8ey@protonmail.com>
2021-12-29 12:23:00 +1100

    attempt 1 to fix chart suite

--- print_checkonchain_charts.py
+++ print_checkonchain_charts.py
@@ -15,25 +15,25 @@ os.chdir('../dcronchain/charts')
 import checkonchain.dcronchain.charts.dcr_charts_plotting
 
 # MONERO
-os.chdir(os.path.dirname(__file__))  # reset dir to this file
-os.chdir('../xmronchain')
-import checkonchain.xmronchain.xmr_charts
+#os.chdir(os.path.dirname(__file__))  # reset dir to this file
+#os.chdir('../xmronchain')
+#import checkonchain.xmronchain.xmr_charts
 
 # ALTCOINS
-os.chdir(os.path.dirname(__file__))  # reset dir to this file
-os.chdir('../altonchain')
-import checkonchain.altonchain.alt_add_metrics
+#os.chdir(os.path.dirname(__file__))  # reset dir to this file
+#os.chdir('../altonchain')
+#import checkonchain.altonchain.alt_add_metrics
 
 # STABLECOINS
-os.chdir(os.path.dirname(__file__))  # reset dir to this file
-os.chdir('../stableonchain')
-import checkonchain.stableonchain.charts.stable_charts_plotting
+#os.chdir(os.path.dirname(__file__))  # reset dir to this file
+#os.chdir('../stableonchain')
+#import checkonchain.stableonchain.charts.stable_charts_plotting
 
 # Print Research Paper Charts
-os.chdir(os.path.dirname(__file__))  # reset dir to this file
-os.chdir('../dcronchain/charts')
-import checkonchain.dcronchain.charts.chart_dcr_unforgeablecostliness
-import checkonchain.dcronchain.charts.chart_dcr_useradoption
+#os.chdir(os.path.dirname(__file__))  # reset dir to this file
+#os.chdir('../dcronchain/charts')
+#import checkonchain.dcronchain.charts.chart_dcr_unforgeablecostliness
+#import checkonchain.dcronchain.charts.chart_dcr_useradoption
 
 
 # Print Last Valid Timestamp
